a9e71aa9d63a7e7a912c8e4badad6857dfa092a0
Hihiz <public@mail.ru>
2024-06-13 17:10:31 +0300

    Создать профиль автомапера для продукта

---
+++ PryanikiTask.Application/Profiles/ProductProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PryanikiTask.Application.Dto.Products;
+using PryanikiTask.Domain.Entities;
+
+namespace PryanikiTask.Application.Profiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, ProductCollectionDto>().ReverseMap();
+        }
+    }
+}
