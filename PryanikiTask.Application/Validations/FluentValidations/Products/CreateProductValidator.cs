2e4d73a2a661bf531cd7f60a0b50a1d8210b84e9
Hihiz <public@mail.ru>
2024-06-13 17:12:43 +0300

    Валидация создания, редактирования продукта

---
+++ PryanikiTask.Application/Validations/FluentValidations/Products/CreateProductValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using PryanikiTask.Application.Dto.Products;
+
+namespace PryanikiTask.Application.Validations.FluentValidations.Products
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDto>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Укажите название продукта !")
+                .MinimumLength(3).WithMessage("Название продукта короткое !");
+            RuleFor(x => x.Price)
+              .NotEmpty().WithMessage("Укажите цену продукта !")
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ PryanikiTask.Application/Validations/FluentValidations/Products/UpdateProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using PryanikiTask.Application.Dto.Products;
+
+namespace PryanikiTask.Application.Validations.FluentValidations.Products
+{
+    public class UpdateProductValidator : AbstractValidator<UpdateProductDto>
+    {
+        public UpdateProductValidator()
+        {
+            RuleFor(x => x.Id)
+              .NotEmpty().WithMessage("Укажите номер продукта ! ");
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Укажите название продукта !")
+                .MinimumLength(3).WithMessage("Название продукта короткое !");
+            RuleFor(x => x.Price)
+              .NotEmpty().WithMessage("Укажите цену продукта !")
+                .GreaterThan(0);
+            RuleFor(x => x.OrderId)
+           .NotEmpty().WithMessage("Укажите заказ !")
+             .GreaterThan(0);
+        }
+    }
+}
