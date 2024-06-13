d4e208679e4f41f416195aad7e4e118ab3d8b2be
Hihiz <public@mail.ru>
2024-06-13 17:07:47 +0300

    Добавить сущность заказа

---
+++ PryanikiTask.Domain/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace PryanikiTask.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
