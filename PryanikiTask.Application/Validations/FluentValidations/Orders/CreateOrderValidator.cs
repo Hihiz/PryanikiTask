afe33f14a2bee3492b58ef592046f5db744892fb
Hihiz <public@mail.ru>
2024-06-13 17:11:46 +0300

    Валидация создания, редактирования заказа

---
+++ PryanikiTask.Application/Validations/FluentValidations/Orders/CreateOrderValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PryanikiTask.Application.Dto.Orders;
+
+namespace PryanikiTask.Application.Validations.FluentValidations.Orders
+{
+    public class CreateOrderValidator : AbstractValidator<CreateOrderDto>
+    {
+        public CreateOrderValidator()
+        {
+            RuleFor(x => x.Description)
+                .MinimumLength(3).WithMessage("Описание заказа короткое !");
+        }
+    }
+}
---
+++ PryanikiTask.Application/Validations/FluentValidations/Orders/UpdateOrderValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using PryanikiTask.Application.Dto.Orders;
+
+namespace PryanikiTask.Application.Validations.FluentValidations.Orders
+{
+    public class UpdateOrderValidator : AbstractValidator<UpdateOrderDto>
+    {
+        public UpdateOrderValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Укажите номер заказа ! ");
+            RuleFor(x => x.Description)
+              .MinimumLength(3).WithMessage("Описание заказа короткое !");
+        }
+    }
+}
