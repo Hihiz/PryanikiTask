5ba0c77b7e0779f086fc14536de8013b717913b9
Hihiz <public@mail.ru>
2024-06-13 17:14:15 +0300

    Создать класс контекста данных

---
+++ PryanikiTask.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PryanikiTask.Application.Interfaces;
+using PryanikiTask.Domain.Entities;
+
+namespace PryanikiTask.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
