68f120680cc5552e74cf6809b56fe4607d78c3d5
Hihiz <public@mail.ru>
2024-06-13 17:17:22 +0300

    Регистрация сервисов

---
+++ PryanikiTask.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PryanikiTask.Application.Interfaces;
+using PryanikiTask.Domain.Entities;
+using PryanikiTask.Infrastructure.Data;
+using PryanikiTask.Infrastructure.Repositories;
+
+namespace PryanikiTask.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                //options.UseNpgsql(connectionString);
+                options.UseInMemoryDatabase("InMemory");
+            });
+
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+            SeedData.Initialize(services.BuildServiceProvider().GetService<ApplicationDbContext>());
+
+            services.RepositoriesInit();
+
+            return services;
+        }
+
+        public static void RepositoriesInit(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<Order>, OrderRepositories>();
+            services.AddScoped<IBaseRepository<Product>, ProductRepositories>();
+        }
+    }
+}
