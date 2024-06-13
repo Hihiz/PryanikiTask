37a39baf38285b186caa96dc414ef7903a556161
Hihiz <public@mail.ru>
2024-06-13 17:10:23 +0300

    Создать профиль автомапера для заказа

---
+++ PryanikiTask.Application/Profiles/OrderProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using PryanikiTask.Application.Dto.Orders;
+using PryanikiTask.Domain.Entities;
+
+namespace PryanikiTask.Application.Profiles
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Order, CreateOrderDto>().ReverseMap();
+            CreateMap<Order, UpdateOrderDto>().ReverseMap();
+        }
+    }
+}
